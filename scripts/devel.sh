341dd8d59b5fdec6a70b676d6c899aedf3c5240f
Eccentric Orange <eccentric.orange2@gmail.com>
2025-08-10 22:19:51 +0530

    use flags

--- scripts/devel.sh
+++ scripts/devel.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
 CONTAINER_NAME="intbrain_harmonic"
+IMAGE_NAME="intbrain-dev-img:harmonic"
+TARGET=$1
+
+if [ -z "$TARGET" ]; then
+    echo "Usage: $0 <target>"
+    echo "Available targets: host, sbc"
+    exit 1
+fi
 
 ./stop.sh
 
+if [ "$TARGET" == "host" ]; then
 
-# Running on the host machine with the robot connected
-docker run -it -d \
---privileged \
---net=host \
---name $CONTAINER_NAME \
---volume $PWD/..:/workspaces/src \
---volume /dev:/dev \
---env="QT_X11_NO_MITSHM=1"  \           
---env="DISPLAY"  \
-intbrain-dev-img:harmonic
+    docker run -it -d \
+    --privileged \
+    --net=host \
+    --name $CONTAINER_NAME \
+    --volume $PWD/..:/workspaces/src \
+    --volume /dev:/dev \
+    --env="DISPLAY"  \
+    --env="QT_X11_NO_MITSHM=1"  \
+    $IMAGE_NAME
 
-# Running on the remote machine with the robot connected
-# docker run -it -d \
-# --privileged \
-# --net=host \
-# --name $CONTAINER_NAME \
-# --volume $PWD/..:/workspaces/src \
-# --volume /dev:/dev \
-# intbrain-dev-img:harmonic
+elif [ "$TARGET" == "sbc" ]; then
+
+    docker run -it -d \
+    --privileged \
+    --net=host \
+    --name $CONTAINER_NAME \
+    --volume $PWD/..:/workspaces/src \
+    --volume /dev:/dev \
+    $IMAGE_NAME
+
+else
+    echo "Invalid target: $TARGET"  
+    echo "Available targets: host, sbc"
+    exit 1
+fi
--- scripts/enter_bash.sh
+++ scripts/enter_bash.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
-# Running on the host machine
-xhost +local:root
-docker exec -it \
---env="DISPLAY"  \
---env="QT_X11_NO_MITSHM=1"  \
---user="ubuntu" \
---workdir="/workspaces" \
-intbrain_harmonic bash
+CONTAINER_NAME="intbrain_harmonic"
+TARGET=$1
 
-# Running on the remote machine
-# docker exec -it \
-# --user="ubuntu" \
-# --workdir="/workspaces" \
-# intbrain_harmonic bash
+if [ -z "$TARGET" ]; then
+    echo "Usage: $0 <target>"
+    echo "Available targets: host, sbc"
+    exit 1
+fi
+
+
+if [ "$TARGET" == "host" ]; then
+
+    xhost +local:root
+    docker exec -it \
+    --env="DISPLAY"  \
+    --env="QT_X11_NO_MITSHM=1"  \
+    --user="ubuntu" \
+    --workdir="/workspaces" \
+    $CONTAINER_NAME bash
+
+elif [ "$TARGET" == "sbc" ]; then
+
+    docker exec -it \
+    --user="ubuntu" \
+    --workdir="/workspaces" \
+    $CONTAINER_NAME bash
+
+else
+    echo "Invalid target: $TARGET"  
+    echo "Available targets: host, sbc"
+    exit 1
+fi
