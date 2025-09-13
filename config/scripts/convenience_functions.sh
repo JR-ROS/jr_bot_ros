c5318b463230cea8ecae5c5c9157f97acdd6db25
Kostubh Khandelwal <khandelwalkostubh@gmail.com>
2025-09-13 19:54:07 +0530

    Add rosdep install alias

--- config/scripts/convenience_functions.sh
+++ config/scripts/convenience_functions.sh
@@ -5,3 +5,6 @@ alias wss="source $WORKSPACE/install/setup.bash && echo \"Sourced workspace\""
 
 # build workspace alias
 alias wsb="cd $WORKSPACE && colcon build --symlink-install && wss"
+
+# god forsaken rosdep install alias
+alias rdi="sudo rosdep install -y --from-paths $WORKSPACE/src --ignore-src --rosdistro $ROS_DISTRO"
